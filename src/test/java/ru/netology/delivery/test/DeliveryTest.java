bf3292971ed8cfd29367e608bddbc90efa44041a
Taake <trollfrost666@gmail.com>
2023-03-27 21:07:31 +0300

    Second

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -12,6 +12,7 @@ import ru.netology.delivery.data.DataGenerator;
 
 import java.time.Duration;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -39,21 +40,21 @@ public class DeliveryTest {
         $("span[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $(byText("Запланировать")).click();
-        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
         $("[data-test-id='success-notification'] .notification__content")
                 .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate))
-                .shouldBe(Condition.visible);
+                .shouldBe(visible);
 
         $("span[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
         $("span[data-test-id=date] input").setValue(secondMeetingDate);
         $(byText("Запланировать")).click();
         $("[data-test-id='replan-notification'] .notification__content")
                 .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
-                .shouldBe(Condition.visible);
+                .shouldBe(visible);
         $("[data-test-id='replan-notification'] button").click();
         $("[data-test-id='success-notification'] .notification__content")
                 .shouldHave(Condition.text("Встреча успешно запланирована на " + secondMeetingDate))
-                .shouldBe(Condition.visible);
+                .shouldBe(visible);
 
 
     }
